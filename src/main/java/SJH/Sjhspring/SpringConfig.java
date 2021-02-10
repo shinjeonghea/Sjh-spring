49e5dbb13d2133a53cc0188897a55c91afcfaa0c
shinjeonghea <58902042+shinjeonghea@users.noreply.github.com>
2021-02-11 01:50:54 +0900

    순수JDBC

--- src/main/java/SJH/Sjhspring/SpringConfig.java
+++ src/main/java/SJH/Sjhspring/SpringConfig.java
@@ -1,22 +1,36 @@
 package SJH.Sjhspring;
 
+import SJH.Sjhspring.repository.JdbcMemberRepository;
 import SJH.Sjhspring.repository.MemberRepository;
 import SJH.Sjhspring.repository.MemoryMemberRepository;
 import SJH.Sjhspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //이건 직접 스프링 빈을 자바로 등록하는 방식
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
