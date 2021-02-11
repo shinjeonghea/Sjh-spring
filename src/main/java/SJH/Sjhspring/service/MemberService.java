f7205c90c2320829e67d994ee5607747a8f0ee9b
shinjeonghea <58902042+shinjeonghea@users.noreply.github.com>
2021-02-11 17:43:47 +0900

    AOP가 필요한 상황 - 모든 메소드의 실행시간을 알고싶을 때

--- src/main/java/SJH/Sjhspring/service/MemberService.java
+++ src/main/java/SJH/Sjhspring/service/MemberService.java
@@ -26,10 +26,19 @@ public class MemberService {
     회원가입
      */
     public Long join(Member member){
-        //같은 이름이 있는 중복회원은 안됨
-        validateDuplicateMember(member); // 중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start= System.currentTimeMillis();
+
+        try {
+            //같은 이름이 있는 중복회원은 안됨
+            validateDuplicateMember(member); // 중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+           long finish =  System.currentTimeMillis();
+           long timeMs = finish - start;
+           System.out.println("join ="+ timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -43,7 +52,15 @@ public class MemberService {
      * 전체 회원 조회
      **/
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+        long start= System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        }finally {
+            long finish =  System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers ="+ timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
